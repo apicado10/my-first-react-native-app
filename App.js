89d536bea00f78911c21c2730a1136f862e6a7f7
Ana Picado <anapicado@Anas-MacBook-Air.local>
2022-08-01 19:06:47 -0600

    bander

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <Text style={[styles.welcome, {backgroundColor: 'blue'}, {color:'white'}, {flex: 1}]}></Text>
+      <Text style={[styles.welcome, {backgroundColor: 'white'}, {flex: 1}]}></Text>
+      <Text style={[styles.welcome, {backgroundColor: 'red'}, {flex: 3}]}></Text>
+      <Text style={[styles.welcome, {backgroundColor: 'white'}, {flex: 1}]}></Text>
+      <Text style={[styles.welcome, {backgroundColor: 'blue'}, {color:'white'}, {flex: 1}]}></Text>
+      <Image style={styles.escudo} source = {require('./src/img/escudo.png')}></Image>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +18,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    flexDirection:'row',
     justifyContent: 'center',
+    alignItems: 'stretch',
+    margin: 10,
   },
+  welcome: {
+    fontSize: 20,
+    alignItems: 'center',
+  },
+  escudo: {
+    width:80,
+    height:80,
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'absolute',
+    transform: [{ rotate: '90deg'}],
+    top: '4',
+  }  
 });
