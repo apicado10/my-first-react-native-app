c22e49b66deccd0f09608e4d0585555e9d14f7a0
Ana Picado <anapicado@Anas-MacBook-Air.local>
2022-08-01 21:59:05 -0600

    trabajo en clase 3

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
@@ -9,7 +9,6 @@ export default function App() {
       <Text style={[styles.welcome, {backgroundColor: 'red'}, {flex: 3}]}></Text>
       <Text style={[styles.welcome, {backgroundColor: 'white'}, {flex: 1}]}></Text>
       <Text style={[styles.welcome, {backgroundColor: 'blue'}, {color:'white'}, {flex: 1}]}></Text>
-      <Image style={styles.escudo} source = {require('./src/img/escudo.png')}></Image>
       <StatusBar style="auto" />
     </View>
   );
@@ -26,14 +25,5 @@ const styles = StyleSheet.create({
   welcome: {
     fontSize: 20,
     alignItems: 'center',
-  },
-  escudo: {
-    width:80,
-    height:80,
-    justifyContent: 'center',
-    alignItems: 'center',
-    position: 'absolute',
-    transform: [{ rotate: '90deg'}],
-    top: '4',
-  }  
+  }, 
 });
